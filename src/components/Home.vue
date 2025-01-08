e2a382a2d5e2f97b5618d9ebf46771aa43b0fc47
arushi-si <arusingh666@gmail.com>
2025-01-08 11:54:26 +0530

    build fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,18 @@
 <script setup>
 import Card from "./Card.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
 
-defineProps({});
+const store = useStore();
+
+const allMissionData = computed(() => store.state.data);
 </script>
 
 <template>
   <ul
     class="list-none grid grid-cols-1 gap-10 px-5 md:grid-cols-3 md:px-20 md:py-10 lg:grid-cols-4 xl:grid-cols-5"
   >
-    <li
-      class="my-5"
-      v-for="mission in this.$store.state.data"
-      :key="mission.id"
-    >
+    <li class="my-5" v-for="mission in allMissionData" :key="mission.id">
       <Card :missionData="mission" />
     </li>
   </ul>
